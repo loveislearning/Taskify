b318c15bfb36781a4c56970491896d7ce9bfa12d
Mew <shallychauhan127@gmail.com>
2025-03-06 19:13:57 +0530

    login page

--- login.js
+++ login.js
@@ -1,11 +1,7 @@
 function toggleForm() {
-    let loginForm = document.querySelector(".login-form");
-    let signUpForm = document.querySelector(".sign-up-form");
-    let forgotPasswordForm = document.querySelector(".forgot-password-form");
-
-    loginForm.style.display = (loginForm.style.display === "none") ? "block" : "none";
-    signUpForm.style.display = (signUpForm.style.display === "none") ? "block" : "none";
-    forgotPasswordForm.style.display = "none";
+    document.querySelector(".login-form").style.display = "none";
+    document.querySelector(".sign-up-form").style.display = "block";
+    document.querySelector(".forgot-password-form").style.display = "none";
 }
 
 function toggleForgotPassword() {
